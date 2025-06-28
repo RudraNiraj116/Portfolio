6ec20896111096735f18aa501121e36d326a679a
Leetcodeniraj <nirajthakurrudra199@gmail.com>
2025-06-28 09:54:38 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
- 
+// js for mobile view or responsive navbar
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
 
-    <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-  integrity="sha512-MD8+PoeVkiG9d4HKOJ2i3j6qIH5zE8WhBhjqh0t91XaaE+JelTYlt91jZWvB2Rktg9+PhmXo9LgDdSJ0WnQePQ=="
-  crossorigin="anonymous"
-  referrerpolicy="no-referrer"
-/>
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("open");
+    navLinks.classList.toggle("active");
+  });
+});
+
+
+
+// Function to animate circular progress and numbers
+function animateCircularSkills() {
+  const circles = document.querySelectorAll('.circle');
+  
+  circles.forEach(circle => {
+    const percent = parseInt(circle.getAttribute('data-percent'));
+    let current = 0;
+    const span = circle.querySelector('span');
+
+    const interval = setInterval(() => {
+      if (current <= percent) {
+        circle.style.setProperty('--value', current);
+        span.textContent = `${current}%`;
+        current++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 20); // Speed of counting
+  });
+}
+
+// Intersection Observer to trigger on scroll into view
+const skillSection = document.querySelector('#Skills');
+let hasAnimated = false;
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && !hasAnimated) {
+      animateCircularSkills();
+      hasAnimated = true;
+      observer.unobserve(skillSection);
+    }
+  });
+}, { threshold: 0.4 });
+
+observer.observe(skillSection);
+
+
+
